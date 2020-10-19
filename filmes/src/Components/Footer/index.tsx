b1a555382c682d82078cd6c68bd1a3e4b9c7db82
Marcos <mtomasferreira@hotmail.com>
2020-10-18 22:35:40 -0300

    fixing css

--- filmes/src/Components/Footer/index.tsx
+++ filmes/src/Components/Footer/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import logo from '../../assets/logonegativo.png';
+
+import './style.css';
+
 export default function Footer(){
     return(
         <div className="footer">
-            <h1>Olá, Pé!</h1>
+            <div className="content">
+                <div className="logo">
+                    <img src={logo} alt="Logomarca"/>
+                </div>
+
+                <div className="info">
+                    <p>
+                        Company Inc., 8901 Marmora Road, Glasgow, D04 89GR 
+                        <br/>
+                        Call us now toll free: (800)2345-6789
+                        <br/>
+                        Customer support: support@demolink.org
+                        <br/>
+                        Skype: sample-username
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
