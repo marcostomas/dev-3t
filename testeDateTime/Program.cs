9457153ba02fcb24e11a87d1bf870c9f21e86a17
Marcos <mtomasferreira@hotmail.com>
2020-11-14 21:14:44 -0300

    consertando a calculadora de data

--- testeDateTime/Program.cs
+++ testeDateTime/Program.cs
@@ -1,62 +1,59 @@
 ﻿using System;
 
-namespace testeDateTime
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Lingugem fdp");
-            DateTime dataInicioEstagio = new DateTime(2019, 9, 10);
+namespace testeDateTime {
+    class Program {
+        static void Main (string[] args) {
+
+            Console.WriteLine ("Coloque a data de início do estágio (DD/MM/AAAA): ");
+
+            DateTime dataInicioEstagio;
             DateTime dataAtual = DateTime.Now;
 
-            var PeriodoDoEstagio = tempoDeEstagio(dataInicioEstagio, dataAtual);
-            if (PeriodoDoEstagio == 1)
-            {
-                Console.WriteLine("{0} Dia", PeriodoDoEstagio);
-            }
-            else if (PeriodoDoEstagio > 1 && PeriodoDoEstagio < 30)
-            {
-                Console.WriteLine("{0} Dias", PeriodoDoEstagio);
-            }
-            else if (PeriodoDoEstagio > 30 && PeriodoDoEstagio < 60)
-            {
-                Console.WriteLine("{0} Dias, ou {1} mês", PeriodoDoEstagio, PeriodoDoEstagio / 30);
-            }
-            else
-            {
-                Console.WriteLine("{0} Dias, ou {1} meses", PeriodoDoEstagio, PeriodoDoEstagio / 30);
-            }
+            if (DateTime.TryParse (Console.ReadLine (), out dataInicioEstagio)) {
+                Console.WriteLine ("A data inserida foi aceita!");
 
+                var PeriodoDoEstagio = tempoDeEstagio (dataInicioEstagio, dataAtual);
 
-            static int tempoDeEstagio(DateTime dataInicioEstagio, DateTime dataAtual)
+                if (PeriodoDoEstagio == 1) 
+                {
+                    
+                } 
+                else if (PeriodoDoEstagio > 1 && PeriodoDoEstagio < 30) 
+                {
+                    Console.WriteLine ("{0} Dias", PeriodoDoEstagio);
+                } 
+                else if (PeriodoDoEstagio > 30 && PeriodoDoEstagio < 60) 
+                {
+                    Console.WriteLine ("{0} Dias, ou {1} mês", PeriodoDoEstagio, PeriodoDoEstagio / 30);
+                } 
+                else if (PeriodoDoEstagio > 365 && PeriodoDoEstagio < 730) 
+                {
+                    Console.WriteLine ("{0} Dias, ou {1} meses, ou {2} ano", PeriodoDoEstagio, PeriodoDoEstagio / 30, PeriodoDoEstagio / 365);
+                } 
+                else 
+                {
+                    Console.WriteLine ("{0} Dias, ou {1} meses, ou {2} anos", PeriodoDoEstagio, PeriodoDoEstagio / 30, PeriodoDoEstagio / 365);
+                }
+
+            } 
+            else 
             {
-
-                long elapsedTicks = dataAtual.Ticks - dataInicioEstagio.Ticks;
-                TimeSpan elapsedSpan = new TimeSpan(elapsedTicks);
-
-                Console.WriteLine("Tempo decorrido desde o começo do estágio em {0:f}:",
-                                   dataAtual.ToShortDateString());
-
-                double tempoEmDiasDouble = elapsedSpan.TotalDays;
-                int tempoEmDiasInt = Convert.ToInt32(tempoEmDiasDouble);
-                return tempoEmDiasInt;
+                Console.WriteLine ("A data inserida não é válida. Verifique se atende ao padrão (DD/MM/AAAA).");
+                Console.WriteLine ("Dica: Coloque as barras, exemplo: 14/11/2020.");
+                System.Threading.Thread.Sleep (7000);
+                Console.Clear ();
             }
+        }
+        static int tempoDeEstagio (DateTime dataInicioEstagio, DateTime dataAtual) {
+
+            long elapsedTicks = dataAtual.Ticks - dataInicioEstagio.Ticks;
+            TimeSpan elapsedSpan = new TimeSpan (elapsedTicks);
+
+            Console.WriteLine ("Tempo decorrido, desde o começo do estágio em {0:f}:", dataInicioEstagio.ToShortDateString ());
+
+            double tempoEmDiasDouble = elapsedSpan.TotalDays;
+            int tempoEmDiasInt = Convert.ToInt32 (tempoEmDiasDouble);
+            return tempoEmDiasInt;
         }
     }
 }
-
-/*
-    int mesAtual = dataAtual.Month;
-    int mesInicio = dataInicioEstagio.Month;
-    int diaAtual = dataAtual.Day;
-    int diaInicio = dataInicioEstagio.Day;
-    int anoAtual = dataAtual.Year;
-    int anoInicio = dataInicioEstagio.Year;
-    int periodoEstagioMes = mesAtual - mesInicio;
-    int periodoEstagioDia = diaAtual - diaInicio;
-    int periodoEstagioAno = anoAtual - anoInicio;
-    TimeSpan periodoEstagio = dataAtual.Subtract(dataInicioEstagio);
-    var teste = periodoEstagio.Ticks;
-    return new Tuple<int, int, int>(periodoEstagioDia, periodoEstagioMes, periodoEstagioAno);
-*/
